57b352d9a0e2b38120980cdfa24bce60614bfda2
lagorg22 <lagorg22@freeuni.edu.ge>
2024-10-08 01:41:06 +0400

    stack impl in c

---
+++ stack_.c
@@ -0,0 +1,57 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    void* elems;
+    int elem_size;
+    int log_len;
+    int alloc_len;
+}Stack;
+
+void stack_init(Stack* s, int elem_size) {
+    s->elem_size = elem_size;
+    s->log_len = 0;
+    s->alloc_len = 5;
+    s->elems = malloc(s->alloc_len * elem_size);
+    assert(s->elems != NULL);
+}
+
+void stack_dispose(Stack* s) {
+    free(s->elems);
+}
+
+void stack_push(Stack* s, void* val) {
+    if(s->log_len == s->alloc_len) {
+        s->alloc_len *= 2;
+        s->elems = realloc(s->elems, s->elem_size * s->alloc_len);
+        assert(s->elems != NULL);
+    }
+    void* last_place = (char*)s->elems + s->log_len * s->elem_size;
+    memcpy(last_place, val, s->elem_size);
+    s->log_len++;
+}
+
+void stack_pop(Stack* s, void* target_ptr) {
+    assert(s->log_len > 0);
+    s->log_len--;
+    void* top_elem = (char*)s->elems + s->log_len * s->elem_size;
+    memcpy(target_ptr, top_elem, s->elem_size);
+}
+
+int main() {
+    Stack s;
+    stack_init(&s, sizeof(int));
+    for(int i = 0; i < 100; i++) {
+        stack_push(&s, &i);
+    }
+    int res;
+    for(int i = 0; i < 11; i++) {
+        stack_pop(&s, &res);
+        printf("%d \n", res);
+    }
+    stack_dispose(&s);
+
+    return 0;
+}
